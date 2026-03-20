d4f426b7900786e03d4d9aa6f96f9c304ba6c7c2
Mudita Chauhan <muditachauhan24@gmail.com>
2026-03-20 23:54:17 +0530

    Improved User Interface

--- frontend/script.js
+++ frontend/script.js
@@ -4,9 +4,15 @@ function addExpense() {
 
   fetch("http://127.0.0.1:5000/add", {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json"
+    },
     body: JSON.stringify({ group, amount })
   })
   .then(res => res.json())
-  .then(data => alert(data.message));
+  .then(data => {
+    alert(data.message);
+    document.getElementById("group").value = "";
+    document.getElementById("amount").value = "";
+  });
 }
