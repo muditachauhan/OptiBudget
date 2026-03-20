a13a680494478a2ba26f358b5b143f8816e4cf47
Mudita Chauhan <muditachauhan24@gmail.com>
2026-03-20 23:33:53 +0530

    Initial project setup

---
+++ frontend/script.js
@@ -0,0 +1,12 @@
+function addExpense() {
+  let group = document.getElementById("group").value;
+  let amount = document.getElementById("amount").value;
+
+  fetch("http://127.0.0.1:5000/add", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ group, amount })
+  })
+  .then(res => res.json())
+  .then(data => alert(data.message));
+}
